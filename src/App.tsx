f4ee90767b753a6d287ae4c05f90f0c4d5dc6752
erzetid <fahrizalm14@gmail.com>
2022-05-26 04:46:32 +0700

    Implement component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Flex,
+  IconButton,
+  useColorMode,
+  useMediaQuery,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  FaSun,
+  FaMoon,
+  FaGithub,
+  FaEnvelope,
+  FaWhatsapp,
+  FaDownload,
+} from "react-icons/fa";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [isNotSmallScreen] = useMediaQuery("(min-width: 768px)");
+  const isDark = colorMode === "dark";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={5}>
+      {isNotSmallScreen ? (
+        <Flex w={"100%"} justify={"center"}>
+          <IconButton
+            aria-label="toggle-icon"
+            icon={isDark ? <FaSun /> : <FaMoon />}
+            onClick={toggleColorMode}
+            isRound
+          />
+        </Flex>
+      ) : null}
+      <Header />
+      <Flex>
+        <IconButton
+          aria-label="git-icon"
+          icon={<FaGithub />}
+          onClick={() => window.open("https://github.com/erzetid")}
+        />
+        <IconButton
+          ml={2}
+          aria-label="google-icon"
+          icon={<FaEnvelope />}
+          onClick={() => window.open("mailto:fahrizalm14@gmail.com")}
+        />
+        <IconButton
+          ml={2}
+          aria-label="whatsapp-icon"
+          icon={<FaWhatsapp />}
+          onClick={() =>
+            window.open("https://api.whatsapp.com/send?phone=62816257921")
+          }
+        />
+        <IconButton
+          ml={2}
+          aria-label="download-icon"
+          icon={<FaDownload />}
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/ambry/?x-li-ambry-ep=AQIqo1ASSALqRgAAAYD9J8XhQR62XG7pXWDAEtnoAjWrBU9mfZF34-gufQz_kyjm-0iEL5gE4sAeMwJ_y-aDSOGfmHhvxyHdSrMr6q0QwsqSgaIJkLe5A5CNfmdfubMBm2ZB5XxrIwwBhdo-C3Otgv2HNbK7Zo9ybZvoKgA8M8br7J5oBnm62hikcoreQF2YEnO1IEVbX7MFRgihhaPzONG9hfePsxFG40-3xCA82YRXpd7025RFqbjYW9WGcbldvIgHnkqtjbYWKTsnckmAbu1E6ORlky4GKRaMt3CbmQsXKOw8dKdhSruMTQYMVEP9Dqa3r6s9BtjnIjHMKBG_6mLPU1czee1EGlyrjQOQj3MGqSELH4bpcQ_Z0wiRwFo_4EvlqTh-SEShldlF977H0Otu5qRJy2Stp083xfaQlJ8iSviFfdKzSisnlgzT1-fwptDg2amcYYaWNto41cZ16omrgXGr8qSWOUPhuvMbL7e3yi2uvSFFuf1Nqq6cCvucu6F3TwFrDUgRyQszbIPtNfllXAy-OmSXUtFp3KVTYH4Q19VeLM6Xx2m-wvwkdtktWly0Gw"
+            )
+          }
+        />
+      </Flex>
+      <Profile />
+      <Footer />
+    </VStack>
   );
 }
 
